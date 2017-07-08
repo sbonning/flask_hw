87714915c8cb60ffe2b30210e3786af4b1c29672
Sam Bonning <sbonning@gmail.com>
2017-07-08 10:00:03 +0530

    with jedi values

--- hello_world.py
+++ hello_world.py
@@ -4,10 +4,35 @@ from os import environ
 app = Flask(__name__)
 
 @app.route("/")
+def say_whatup():
+	return "Whatup!"
+
 @app.route("/hello")
 def say_hi():
 	return "Hello World!"
 
+@app.route("/hello/<name>")
+def hello_person(name):
+    html = """
+        <h1>
+            Hello {}!
+        </h1>
+        <p>
+            Here's a picture of a kitten.  Awww...
+        </p>
+        <img src="http://kittenrescue.org/wp-content/uploads/2017/03/KittenRescue_KittenCareHandbook.jpg">
+    """
+    return html.format(name.title())
+
+@app.route("/jedi/<first>/<last>")
+def jedi_name(first,last):
+	html = """
+		<h1>
+			Your Jedi name is {0}{1}
+		</h1>
+	"""
+	return html.format(last[0:3],first[0:3])
+
+
 if __name__ == "__main__":
-	app.run(host=environ['IP'],
-		port=int(environ['PORT']))
+	app.run()
